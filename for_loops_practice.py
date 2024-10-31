96160599268723f6503e9481aa67c9d5740d5b34
Olvera <aolvera22@cps.k12.il.us>
2024-10-31 09:14:24 -0500

    Week_10_stuff

--- for_loops_practice.py
+++ for_loops_practice.py
@@ -2,9 +2,14 @@
 horror_characters = {"Freddy Krueger", "Jason Voorhees", "Michael Myers", "Pennywise", "Chucky"}
 # Iterate though the list and print each character 
 
-for x in horror_characters:
-    if x == "Michael Myers":
-        x = "Dracula"
-    if x == "Pennywise":
-        x = "Human Centipede"
+
+
+
+horror_movies = {"Child's Play 2", "The Shining", "The Fly", "A Nightmare on Elm Street", "Halloween", "Scream"}
+
+for x in horror_movies:
+    if x == "Child's Play 2":
+        continue
+    if x == "The Fly":
+        x = "Saw"
     print(x)
